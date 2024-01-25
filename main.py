b19abc975bbd53fc90b05db1d79a11ad48034701
SardineMilk <152029833+SardineMilk@users.noreply.github.com>
2024-01-25 19:21:59 +0000

    Update main.py
    pull neural network out to a class

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 """
 There are 3 layers, Input, Hidden and Output
@@ -12,24 +13,25 @@ https://www.youtube.com/watch?v=9RN2Wr8xvro was used as the main learning resour
 """
 
 
-def forward_propagation(layer_input_values, weights, biases):
-    # Takes an array of input values, multiplies by an array of weights, adds the biases, normalises it and returns it
-    layer_output_values = biases + (weights @ layer_input_values)  # "@" is __matmul__, designed for matrix multiplication
-    layer_output_values_normalised = sigmoid(layer_output_values)  # Normalise the hidden_pre values using Sigmoid function
-    return layer_output_values_normalised
+# Initialises a neural network with a defined shape
+class NeuralNetwork:
+    def __init__(self, network_shape):
+        self.layers_number = len(network_shape)
+        # Initialise the weights and biases
+        self.weights = []
+        self.biases = []
 
+        for layer_index in range(len(network_shape) - 1):
+            first_layer = network_shape[layer_index]
+            second_layer = network_shape[layer_index + 1]
 
-def layer_delta_error(layer_index, last_delta_error):
-    # How much each neuron participated towards the error in the output
-    current_layer = layer_outputs[layer_index]
-    sigmoid_derivative = current_layer * (1 - current_layer)
-    delta_error = np.transpose(network_weights[layer_index + 1]) @ last_delta_error * sigmoid_derivative
-    return delta_error
+            # Weights from the first to the second layer
+            layer_weights = np.random.uniform(-0.5, 0.5, (second_layer, first_layer))
+            # Biases added to the second layer
+            layer_biases = np.zeros((second_layer, 1))
 
-
-def sigmoid(array):
-    # Performs Sigmoid function on all elements of a numpy array
-    return 1 / (1 + np.exp(-array))
+            self.weights.append(layer_weights)
+            self.biases.append(layer_biases)
 
 
 def get_mnist(file_path):
@@ -43,8 +45,7 @@ def get_mnist(file_path):
     # Normalise pixel values to be between 0 and 1
     mnist_images = mnist_images.astype("float32") / 255
 
-    # Reshape from a 2d 28*28 array to a 1d 784 array
-    # So the network can take it as input
+    # Reshape from a 28*28 array to a 784*1 array so the network can take it as an input
     mnist_images = np.reshape(mnist_images, (mnist_images.shape[0], mnist_images.shape[1] * mnist_images.shape[2]))
 
     # Convert labels to one-hot encoding, so they can be checked against the network output
@@ -55,32 +56,38 @@ def get_mnist(file_path):
     return mnist_images, mnist_labels
 
 
+def forward_propagation(layer_input_values, weights, biases):
+    # Takes an array of input values, multiplies by an array of weights, adds the biases, normalises it and returns it
+    layer_output_values = biases + (weights @ layer_input_values)  # "@" is __matmul__, designed for matrix multiplication
+    layer_output_values_normalised = sigmoid(layer_output_values)  # Normalise the hidden_pre values using Sigmoid function.
+    return layer_output_values_normalised
+
+
+def layer_delta_error(layer_index, last_delta_error):
+    # How much each neuron participated towards the error in the output
+    current_layer = layer_values[layer_index]
+    sigmoid_derivative = current_layer * (1 - current_layer)
+    error = np.transpose(network.weights[layer_index + 1]) @ last_delta_error * sigmoid_derivative
+    return error
+
+
+def sigmoid(array):
+    # There are different / better functions: investigate Leaky RELU
+    # Performs Sigmoid function on all elements of a numpy array
+    return 1 / (1 + np.exp(-array))
+
+
 # images is a (60000, 784) array of values ranging from 0 to 1
 # labels is a (60000, 10) array with a single 1 and nine 0's
 images, labels = get_mnist("mnist.npz")
 
 # Set up the layer sizes
-network_structure = [784, 40, 10]
-# learn_rate is the effect that errors have on the weights and biases.
-learn_rate = 0.01
+network_structure = [784, 25, 10]
+learn_rate = 0.015  # learn_rate is the effect that errors have on the weights and biases.
 training_generations = 5
 
-layers_number = len(network_structure)
-# Initialise the weights and biases
-network_weights = []  # 0 = input -> hidden, 1 = hidden -> output
-network_biases = []  # 0 = hidden, 1 = output
-for i in range(len(network_structure) - 1):
-    first_layer = network_structure[i]
-    second_layer = network_structure[i+1]
-
-    # Weights from the first to the second layer
-    layer_weights = np.random.uniform(-0.5, 0.5, (second_layer, first_layer))
-    # Biases added to the second layer
-    # layer_biases = np.zeros((second_layer, 1))
-    layer_biases = np.zeros((second_layer, 1))
-
-    network_weights.append(layer_weights)
-    network_biases.append(layer_biases)
+# Initialise a network
+network = NeuralNetwork(network_structure)
 
 # Train the network
 for generation in range(training_generations):  # Iterate for several generations
@@ -94,70 +101,72 @@ for generation in range(training_generations):  # Iterate for several generation
         image.shape += (1,)
         label.shape += (1,)
 
-        # hidden_values = process_layer(image, weight_input_hidden, bias_input_hidden)
-        # output_values = process_layer(hidden_values, weight_hidden_output, bias_hidden_output)
-
         # Forward propagation
-        layer_outputs = []
-        input_values = image
-
-        for layer in range(layers_number - 1):  # -1 is because the output values don't need processed
+        layer_values = [image]
+        for layer in range(network.layers_number - 1):  # -1 is because the output values don't need processed
             # Perform the matrix multiplication on the last layer's data
-            output_values = forward_propagation(input_values, network_weights[layer], network_biases[layer])
-            input_values = output_values
-            layer_outputs.append(output_values)
+            output_values = forward_propagation(layer_values[-1], network.weights[layer], network.biases[layer])
+            layer_values.append(output_values)
 
-        hidden_values = layer_outputs[0]
+        # Back propagation
+        for j in range(network.layers_number - 1):
+            layer = -(j + 1)
+            # Error calculation - how much did each neuron contribute to the error in the output
+            if layer == -1:
+                delta_error = (layer_values[layer] - label)
+            else:
+                # noinspection PyUnboundLocalVariable
+                delta_error = layer_delta_error(layer, delta_error)
 
-        # Back propagation output -> hidden
-        delta_output = layer_outputs[-1] - label  # How wrong was the output?
-        # Adjust the weights and biases based on delta_output and learn_rate - gradient descent
-        network_weights[1] += -learn_rate * delta_output @ np.transpose(hidden_values)  # Update hidden -> output weights using delta output and learn rate
-        network_biases[1] += -learn_rate * delta_output  # Update all biases for hidden -> output layer
-        # Back propagation hidden -> input
-        delta_hidden = layer_delta_error(0, delta_output)
-        network_weights[0] += -learn_rate * delta_hidden @ np.transpose(image)
-        network_biases[0] += -learn_rate * delta_hidden
+            # Gradient descent - move towards the local minimum
+            network.weights[layer] += -learn_rate * delta_error @ np.transpose(layer_values[layer - 1])
+            network.biases[layer] += -learn_rate * delta_error
 
-        # Cost / Error calculation
-        # It is just used by the user, not the program
-        # This is the mean squared error formula. It returns the average error of the entire network
-        # Calculates the difference between each output and the corresponding label and squares it then sums up all the values and divides by the total number of output neurons
-        average_error = 1 / network_structure[-1] * np.sum((layer_outputs[-1] - label) ** 2, axis=0)
+        # Cost / Error calculation - not used in program
+        # This is the mean squared error formula. It returns the average error of the entire network's output
+        # Calculates the difference between output and label, squares it, sums up all the values and divides by the total number of output neurons
+        average_error = 1 / network_structure[-1] * np.sum((layer_values[-1] - label) ** 2, axis=0)
         # Checks if the input was classified correctly
-        # Is the highest output neuron the same as the label and if it is, add 1 to number_correct
-        number_correct += int(np.argmax(layer_outputs[-1]) == np.argmax(label))
+        is_correct = np.argmax(layer_values[-1]) == np.argmax(label)
+        number_correct += int(is_correct)
 
     accuracy = (number_correct / 50000) * 100
     print(f"Out of 50000 training images, {number_correct} were correctly classified, at an accuracy of {round(accuracy, 2)}%")
 
-"""
-with open("network_weights", "w") as file:
-    for layer in network_weights:
-        np.savetxt(file, layer)
-with open("network_biases", "w") as file:
-    for layer in network_biases:
-        np.savetxt(file, layer)
-"""
-
-# Test the network
+# Automatically test the network
 number_correct = 0
 for i in range(50000, 60000):
     image = images[i]
     image.shape += (1,)
 
     # Forward propagation
-    layer_outputs = []
-    input_values = image
+    layer_values = [image]
+    for layer in range(network.layers_number - 1):  # -1 is because the output values don't need processed
+        # Perform the matrix multiplication on the last layer's data
+        output_values = forward_propagation(layer_values[-1], network.weights[layer], network.biases[layer])
+        layer_values.append(output_values)
+    output = np.argmax(layer_values[-1])
 
-    for layer in range(layers_number - 1):  # -1 is because the output values don't need processed
-        # Perform the matrix multiplication on the output from the last layer
-        output_values = forward_propagation(input_values, network_weights[layer], network_biases[layer])
-        input_values = output_values
-        layer_outputs.append(output_values)
+    is_correct = output == np.argmax(labels[i])
+    number_correct += int(is_correct)
 
-    output = np.argmax(layer_outputs[-1])
-    if output == np.argmax(labels[i]):
-        number_correct += 1
 accuracy = (number_correct / 10000) * 100
 print(f"Out of 10000 new test images, {number_correct} were correctly identified, at an accuracy of {round(accuracy, 2)}%")
+
+# User test
+while True:
+    index = int(input("Enter a number (0 - 59999): "))
+    image = images[index]
+    image.shape += (1,)
+
+    # Forward propagation
+    layer_values = [image]
+    for layer in range(network.layers_number - 1):  # -1 is because the output values don't need processed
+        # Perform the matrix multiplication on the last layer's data
+        output_values = forward_propagation(layer_values[-1], network.weights[layer], network.biases[layer])
+        layer_values.append(output_values)
+
+    output = np.argmax(layer_values[-1])
+    plt.imshow(image.reshape(28, 28), cmap="Greys")
+    plt.title(f"Label = {np.argmax(labels[index])}. Output = {output}")
+    plt.show()
